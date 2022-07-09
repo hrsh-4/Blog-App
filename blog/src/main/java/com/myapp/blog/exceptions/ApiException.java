0814a03bfd83b32064fde93d741233791961dbc1
Harsh Kushwah <49446555+hrsh-4@users.noreply.github.com>
2022-07-09 14:58:49 +0530

    Add files via upload

---
+++ blog/src/main/java/com/myapp/blog/exceptions/ApiException.java
@@ -0,0 +1,15 @@
+package com.myapp.blog.exceptions;
+
+public class ApiException extends RuntimeException {
+
+	public ApiException(String message) {
+		super(message);
+
+	}
+
+	public ApiException() {
+		super();
+
+	}
+
+}
